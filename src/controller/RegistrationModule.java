0127ef831332907033531be727b7e4d2b4273b60
Niveth <nivethsaran@gmail.com>
2018-10-09 23:07:10 +0530

    Dev Phase Minor Update

--- src/controller/RegistrationModule.java
+++ src/controller/RegistrationModule.java
@@ -2,5 +2,9 @@ package controller;
 
 public class RegistrationModule {
 	
-
+void registrationSetup()
+{//String  
+	System.out.println("SNAAP Password Manager to save all your passwords Hassle Free\n"
+			+ "Please enter details as requested");
+}
 }
